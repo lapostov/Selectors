7424dc93bcc5c872f1611f00b4043dd1a245eaf2
Александр Масляков <lapostov@yandex.ru>
2022-12-05 00:51:07 +0400

    Added new command

--- src/test/java/SelectorsTest.java
+++ src/test/java/SelectorsTest.java
@@ -46,7 +46,7 @@ public class SelectorsTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79271620965");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
