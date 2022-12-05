8d289f60a8c5f9bba0ebe2eb5c51ee5d1c64885e
Александр Масляков <lapostov@yandex.ru>
2022-12-05 13:59:58 +0400

    Deleted metod setup

--- src/test/java/SelectorsTest.java
+++ src/test/java/SelectorsTest.java
@@ -20,11 +20,6 @@ public class SelectorsTest {
     }
 
     @BeforeEach
-    void setup() {
-        driver = new ChromeDriver();
-    }
-
-    @BeforeEach
     void option() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
@@ -46,7 +41,7 @@ public class SelectorsTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79271620965");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
